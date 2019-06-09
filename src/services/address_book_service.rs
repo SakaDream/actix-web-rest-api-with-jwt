c8d8e78f6cd1e3f7a712aea37778419072b77951
SakaDream <bahaimt@gmail.com>
2019-06-09 09:54:09 +0700

    Sync with actix-jwt branch

--- src/services/address_book_service.rs
+++ src/services/address_book_service.rs
@@ -17,11 +17,8 @@ pub fn find_all(pool: &web::Data<Pool>) -> Result<Vec<Person>, ServiceError> {
 
 pub fn find_by_id(id: i32, pool: &web::Data<Pool>) -> Result<Person, ServiceError> {
     match Person::find_by_id(id, &pool.get().unwrap()) {
-        Ok(person_option) => match person_option {
-            Some(person) => Ok(person),
-            None => Err(ServiceError::new(StatusCode::NOT_FOUND, format!("Person with id {} not found", id))),
-        },
-        Err(_) => Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, constants::MESSAGE_CAN_NOT_FETCH_DATA.to_string())),
+        Ok(person) => Ok(person),
+        Err(_) => Err(ServiceError::new(StatusCode::NOT_FOUND, format!("Person with id {} not found", id))),
     }
 }
 
@@ -32,39 +29,29 @@ pub fn query(query: String, pool: &web::Data<Pool>) -> Result<Vec<Person>, Servi
     }
 }
 
-pub fn insert(new_person: PersonDTO, pool: &web::Data<Pool>) -> Result<String, ServiceError> {
+pub fn insert(new_person: PersonDTO, pool: &web::Data<Pool>) -> Result<(), ServiceError> {
     match Person::insert(new_person, &pool.get().unwrap()) {
-        Ok(_) => Ok(constants::MESSAGE_OK.to_string()),
+        Ok(_) => Ok(()),
         Err(_) => Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, constants::MESSAGE_CAN_NOT_INSERT_DATA.to_string())),
     }
 }
 
-pub fn update(id: i32, updated_person: PersonDTO, pool: &web::Data<Pool>) -> Result<String, ServiceError> {
+pub fn update(id: i32, updated_person: PersonDTO, pool: &web::Data<Pool>) -> Result<(), ServiceError> {
     match Person::find_by_id(id, &pool.get().unwrap()) {
-        Ok(person_option) => match person_option {
-            Some(_) => {
-                match Person::update(id, updated_person, &pool.get().unwrap()) {
-                    Ok(_) => Ok(constants::MESSAGE_OK.to_string()),
-                    Err(_) => Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, constants::MESSAGE_CAN_NOT_UPDATE_DATA.to_string())),
-                }
-            },
-            None => Err(ServiceError::new(StatusCode::NOT_FOUND, format!("Person with id {} not found", id))),
+        Ok(_) => match Person::update(id, updated_person, &pool.get().unwrap()) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, constants::MESSAGE_CAN_NOT_UPDATE_DATA.to_string())),
         },
-        Err(_) => Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, constants::MESSAGE_CAN_NOT_FETCH_DATA.to_string())),
+        Err(_) => Err(ServiceError::new(StatusCode::NOT_FOUND, format!("Person with id {} not found", id))),
     }
 }
 
-pub fn delete(id: i32, pool: &web::Data<Pool>) -> Result<String, ServiceError> {
+pub fn delete(id: i32, pool: &web::Data<Pool>) -> Result<(), ServiceError> {
     match Person::find_by_id(id, &pool.get().unwrap()) {
-        Ok(person_option) => match person_option {
-            Some(_) => {
-                match Person::delete(id, &pool.get().unwrap()) {
-                    Ok(_) => Ok(constants::MESSAGE_OK.to_string()),
-                    Err(_) => Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, constants::MESSAGE_CAN_NOT_DELETE_DATA.to_string())),
-                }
-            },
-            None => Err(ServiceError::new(StatusCode::NOT_FOUND, format!("Person with id {} not found", id))),
+        Ok(_) => match Person::delete(id, &pool.get().unwrap()) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, constants::MESSAGE_CAN_NOT_DELETE_DATA.to_string())),
         },
-        Err(_) => Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, constants::MESSAGE_CAN_NOT_FETCH_DATA.to_string())),
+        Err(_) => Err(ServiceError::new(StatusCode::NOT_FOUND, format!("Person with id {} not found", id))),
     }
 }
